04bf0886307db68f597c22c848ec6176ec957c5a
Amitay <emhamitay@gmail.com>
2026-01-03 23:38:53 +0200

    updated the link to github

--- client/src/features/Information.jsx
+++ client/src/features/Information.jsx
@@ -1,39 +1,40 @@
 import { Separator } from "@/components/ui/separator";
 import Link from "./Information/Link";
 
-export default function Information(){
-    return (
-        <div className="font-sans antialiased">
-            <h1 className="text-base font-bold text-blue-950">Information</h1>
-            <Separator />
-            <Link 
-                src='https://github.com/emhamitay/funtodo-fullstack' 
-                type='github' 
-                pre="View the complete source code on "
-            >
-                FunTodo Fullstack Repository
-            </Link>
-            <Link 
-                src='https://github.com/emhamitay/TutorialFramework' 
-                type='github' 
-                pre="Explore the tutorial framework at "
-            >
-                Tutorial Framework
-            </Link>
-            <Link 
-                src='https://github.com/emhamitay/dnd-framework--w-example' 
-                type='github' 
-                pre="Check out the drag and drop framework at "
-            >
-                Drag & Drop Framework
-            </Link>
-            <Link 
-                src='https://www.npmjs.com/package/bhi-dnd' 
-                type='npm' 
-                pre="Available on npm as "
-            >
-               {" "}bhi-dnd
-            </Link>
-        </div>
-    );
+export default function Information() {
+  return (
+    <div className="font-sans antialiased">
+      <h1 className="text-base font-bold text-blue-950">Information</h1>
+      <Separator />
+      <Link
+        src="https://github.com/emhamitay/funtodo"
+        type="github"
+        pre="View the complete source code on "
+      >
+        FunTodo Fullstack Repository
+      </Link>
+      <Link
+        src="https://github.com/emhamitay/TutorialFramework"
+        type="github"
+        pre="Explore the tutorial framework at "
+      >
+        Tutorial Framework
+      </Link>
+      <Link
+        src="https://github.com/emhamitay/dnd-framework--w-example"
+        type="github"
+        pre="Check out the drag and drop framework at "
+      >
+        Drag & Drop Framework
+      </Link>
+      <Link
+        src="https://www.npmjs.com/package/bhi-dnd"
+        type="npm"
+        pre="Available on npm as "
+      >
+        {" "}
+        bhi-dnd
+      </Link>
+    </div>
+  );
 }
